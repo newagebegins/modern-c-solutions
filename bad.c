9a5823d119839080067f9025ad9780087534584d
newagebegins <newagebegins@gmail.com>
2023-03-27 08:30:46 +1000

    Add bad.c

---
+++ bad.c
@@ -0,0 +1,23 @@
+/* This may look like nonsense, but really is -*- mode: C -*- */
+
+/* The main thing that this program does. */
+void main(void) {
+  // Declarations
+  int i;
+  double A[5] = {
+    9.0,
+    2.9,
+    3.E+25,
+    .00007,
+  };
+
+  // Doing some work
+  for (i = 0; i < 5; ++i) {
+    printf("element %d is %g, \tits square is %g\n",
+           i,
+           A[i],
+           A[i]*A[i]);
+  }
+
+  return 0;
+}

d1a17fcac38c91e4485e0bcaba6634e48d01f6e3
newagebegins <newagebegins@gmail.com>
2023-03-28 20:22:19 +1000

    Implement quick sort for Challenge 1

--- challenge1.c
+++ challenge1.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+// ============== Merge Sort =====================
+
 void CopyArray(double A[], int iBegin, int iEnd, double B[]) {
   for (int k = iBegin; k < iEnd; ++k) {
     B[k] = A[k];
@@ -37,6 +39,42 @@ void TopDownMergeSort(double A[], double B[], int n) {
   TopDownSplitMerge(B, 0, n, A);
 }
 
+// ============== Quick Sort ===========================
+
+int partition(double A[], int lo, int hi) {
+  double pivot = A[hi];
+
+  int i = lo - 1;
+
+  for (int j = lo; j < hi; ++j) {
+    if (A[j] <= pivot) {
+      ++i;
+      double tmp = A[i];
+      A[i] = A[j];
+      A[j] = tmp;
+    }
+  }
+
+  ++i;
+  double tmp = A[i];
+  A[i] = A[hi];
+  A[hi] = tmp;
+  return i;
+}
+
+void quicksort(double A[], int lo, int hi) {
+  if (lo >= hi || lo < 0) {
+    return;
+  }
+
+  int p = partition(A, lo, hi);
+
+  quicksort(A, lo, p - 1);
+  quicksort(A, p + 1, hi);
+}
+
+// =====================================================
+
 void printArray(double A[], size_t n) {
   printf("[");
   if (n == 0) {
@@ -53,7 +91,14 @@ int main(void) {
   double A[8] = {8.0, 3.3, 5.2, 3.3, 9.0, 22.1, 7.11, 0.3};
   double B[8];
 
+  printf("Merge sort:\n");
   printArray(A, 8);
   TopDownMergeSort(A, B, 8);
   printArray(A, 8);
+
+  double C[8] = {8.0, 3.3, 5.2, 3.3, 9.0, 22.1, 7.11, 0.3};
+  printf("\nQuick sort:\n");
+  printArray(C, 8);
+  quicksort(C, 0, 7);
+  printArray(C, 8);
 }

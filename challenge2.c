3db2ca6434c61de230b74b3c3f424f418ca874c2
newagebegins <newagebegins@gmail.com>
2023-03-29 16:05:59 +1000

    Derivative function

--- challenge2.c
+++ challenge2.c
@@ -4,19 +4,23 @@
 
 static double const dx = 1E-10;
 
+double derivative(double F(double), double x) {
+  return (F(x+dx) - F(x))/dx;
+}
+
 int main(void) {
   double x, der, ref;
   x = 3.5;
 
-  der = (sin(x+dx) - sin(x))/dx;
+  der = derivative(sin, x);
   ref = cos(x);
   printf("sin(x): %.10f\t%.10f\n", der, ref);
 
-  der = (cos(x+dx) - cos(x))/dx;
+  der = derivative(cos, x);
   ref = -sin(x);
   printf("cos(x): %.10f\t%.10f\n", der, ref);
 
-  der = (sqrt(x+dx) - sqrt(x))/dx;
+  der = derivative(sqrt, x);
   ref = 1/(2*sqrt(x));
   printf("sqrt(x): %.10f\t%.10f\n", der, ref);
 

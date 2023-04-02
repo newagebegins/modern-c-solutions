86a7b9b2100157b99e7a73b1bc1464517bc41ea7
newagebegins <newagebegins@gmail.com>
2023-04-02 13:15:57 +1000

    Count connected components

---
+++ ch7-connected-components.c
@@ -0,0 +1,76 @@
+#include <stdbool.h>
+#include <assert.h>
+#include <stdio.h>
+
+#define NUM_NODES 7
+
+bool visited[NUM_NODES] = {0};
+bool adjacency_matrix[NUM_NODES][NUM_NODES] = {
+  {0,0,0,0,0,0,0},
+  {0,0,1,0,1,0,0},
+  {0,1,0,1,0,0,0},
+  {0,0,1,0,0,0,0},
+  {0,1,0,0,0,0,0},
+  {0,0,0,0,0,0,1},
+  {0,0,0,0,0,1,0},
+};
+
+unsigned queue[NUM_NODES] = {0};
+int front = -1;
+int rear = -1;
+
+bool is_empty() {
+  return front == -1;
+}
+
+bool is_full() {
+  return (rear + 1) % NUM_NODES == front;
+}
+
+void enqueue(unsigned x) {
+  assert(!is_full());
+  if (is_empty()) {
+    front = 0;
+    rear = 0;
+  } else {
+    rear = (rear + 1) % NUM_NODES;
+  }
+  queue[rear] = x;
+}
+
+unsigned dequeue() {
+  assert(!is_empty());
+  unsigned x = queue[front];
+  if (front == rear) {
+    front = -1;
+    rear = -1;
+  } else {
+    front = (front + 1) % NUM_NODES;
+  }
+  return x;
+}
+
+void breadth_first_search() {
+  while (!is_empty()) {
+    unsigned x = dequeue();
+    visited[x] = true;
+    for (unsigned n = 0; n < NUM_NODES; ++n) {
+      if (adjacency_matrix[x][n] && !visited[n]) {
+        enqueue(n);
+      }
+    }
+  }
+}
+
+int main(void) {
+  unsigned components = 0;
+  for (unsigned n = 0; n < NUM_NODES; ++n) {
+    if (!visited[n]) {
+      enqueue(n);
+      breadth_first_search();
+      ++components;
+    }
+  }
+  printf("The graph contains %u components\n", components);
+  return 0;
+}

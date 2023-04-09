052a3c3e3d1c491cf6b22d0958805727aebc77f3
newagebegins <newagebegins@gmail.com>
2023-04-09 14:00:20 +1000

    Exs 16: Implement rat_dotproduct

--- rationals.c
+++ rationals.c
@@ -71,6 +71,29 @@ rat* rat_extend(rat* rp, size_t f) {
   return rp;
 }
 
+rat* rat_sumup(rat* rp, rat y) {
+  size_t c = gcd(rp->denom, y.denom);
+  size_t ax = y.denom/c;
+  size_t bx = rp->denom/c;
+  rat_extend(rp, ax);              /*@\label{rat_extend_x}*/
+  y = rat_get_extended(y, bx);
+  assert(rp->denom == y.denom);
+
+  if (rp->sign == y.sign) {
+    rp->num += y.num;
+  } else if (rp->num > y.num) {
+    rp->num -= y.num;
+  } else {
+    rp->num = y.num - rp->num;
+    rp->sign = !rp->sign;
+  }
+  return rat_normalize(rp);       /*@\label{rat_normalize_x}*/
+}
+
+rat* rat_rma(rat* rp, rat x, rat y) {
+  return rat_sumup(rp, rat_get_prod(x, y));
+}
+
 // Exs 14, p.152
 char const* rat_print(size_t len, char tmp[len], rat const* x) {
   if (len > 0) {
@@ -86,6 +109,15 @@ char const* rat_normalize_print(size_t len, char tmp[len], rat const* x) {
   return rat_print(len, tmp, x);
 }
 
+// Exs 16
+rat* rat_dotproduct(rat rp[static 1], size_t n, rat const A[n], rat const B[n]) {
+  rat_init(rp, 0, 1);
+  for (size_t i = 0; i < n; ++i) {
+    rat_rma(rp, A[i], B[i]);
+  }
+  return rp;
+}
+
 int main(void) {
   char buf[16];
   rat r1 = rat_get(11, 7);
@@ -94,5 +126,23 @@ int main(void) {
   printf("r2 = %s\n", rat_print(16, buf, &r2));
   rat r3 = rat_get(-25, 100);
   printf("r3 = %s\n", rat_normalize_print(16, buf, &r3));
+
+  rat A[3];
+  A[0] = rat_get(1, 3);
+  A[1] = rat_get(-4, 8);
+  A[2] = rat_get(25, 3);
+
+  rat B[3];
+  B[0] = rat_get(5, 7);
+  B[1] = rat_get(5, 77);
+  B[2] = rat_get(-64, 8);
+
+  rat dp = {0};
+  rat_dotproduct(&dp, 3, A, B);
+  assert(dp.sign);
+  assert(dp.num == 10235);
+  assert(dp.denom == 154);
+  printf("dp = %s\n", rat_print(16, buf, &dp));
+
   return EXIT_SUCCESS;
 }

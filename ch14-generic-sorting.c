7c9c95b9cd83f47a62612a3408cb57f4ef4b8f18
newagebegins <newagebegins@gmail.com>
2023-04-10 18:11:42 +1000

    Challenge 14: Performance comparison

--- ch14-generic-sorting.c
+++ ch14-generic-sorting.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
+#include <time.h>
+#include <assert.h>
 
 typedef int compare_function(void const*, void const*);
 
@@ -109,6 +111,15 @@ void printArray(double A[], size_t n) {
   }
 }
 
+void fill_with_random_numbers(double A[], size_t size) {
+  double f = 1000.0 / RAND_MAX;
+  for (size_t i = 0; i < size; ++i) {
+    A[i] = rand() * f;
+  }
+}
+
+enum { S1 = 5000000, S2 = S1*3 };
+
 int main(void) {
   double A[8] = {8.0, 3.3, 5.2, 3.3, 9.0, 22.1, 7.11, 0.3};
   double B[8];
@@ -123,4 +134,71 @@ int main(void) {
   printArray(C, 8);
   quicksort(C, 0, 7, sizeof C[0], compare_double);
   printArray(C, 8);
+
+  printf("\n ===== Performance comparison =====\n\n");
+
+  clock_t c1, c2;
+  size_t t;
+
+  printf("Merge sort:\n");
+
+  static double A1[S1];
+  static double B1[S1];
+  fill_with_random_numbers(A1, S1);
+  c1 = clock();
+  TopDownMergeSort(A1, B1, S1, sizeof(double), compare_double);
+  c2 = clock();
+  t = (c2 - c1) * 1000 / CLOCKS_PER_SEC;
+  printf("%d items -> %zu ms\n", S1, t);
+  assert(isSorted(A1, S1));
+
+  static double A2[S2];
+  static double B2[S2];
+  fill_with_random_numbers(A2, S2);
+  c1 = clock();
+  TopDownMergeSort(A2, B2, S2, sizeof(double), compare_double);
+  c2 = clock();
+  t = (c2 - c1) * 1000 / CLOCKS_PER_SEC;
+  printf("%d items -> %zu ms\n", S2, t);
+  assert(isSorted(A2, S2));
+
+  printf("\nQuick sort:\n");
+
+  static double C1[S1];
+  fill_with_random_numbers(C1, S1);
+  c1 = clock();
+  quicksort(C1, 0, S1-1, sizeof(double), compare_double);
+  c2 = clock();
+  t = (c2 - c1) * 1000 / CLOCKS_PER_SEC;
+  printf("%d items -> %zu ms\n", S1, t);
+  assert(isSorted(C1, S1));
+
+  static double C2[S2];
+  fill_with_random_numbers(C2, S2);
+  c1 = clock();
+  quicksort(C2, 0, S2-1, sizeof(double), compare_double);
+  c2 = clock();
+  t = (c2 - c1) * 1000 / CLOCKS_PER_SEC;
+  printf("%d items -> %zu ms\n", S2, t);
+  assert(isSorted(C2, S2));
+
+  printf("\nLibc quick sort:\n");
+
+  fill_with_random_numbers(C1, S1);
+  c1 = clock();
+  qsort(C1, S1, sizeof(double), compare_double);
+  c2 = clock();
+  t = (c2 - c1) * 1000 / CLOCKS_PER_SEC;
+  printf("%d items -> %zu ms\n", S1, t);
+  assert(isSorted(C1, S1));
+
+  fill_with_random_numbers(C2, S2);
+  c1 = clock();
+  qsort(C2, S2, sizeof(double), compare_double);
+  c2 = clock();
+  t = (c2 - c1) * 1000 / CLOCKS_PER_SEC;
+  printf("%d items -> %zu ms\n", S2, t);
+  assert(isSorted(C2, S2));
+
+  return EXIT_SUCCESS;
 }

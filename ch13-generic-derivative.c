8c0363737a5a8d69d48975b64e00e59805d4510e
newagebegins <newagebegins@gmail.com>
2023-04-10 13:26:41 +1000

    Find a complex zero of a polynomial

--- ch13-generic-derivative.c
+++ ch13-generic-derivative.c
@@ -5,10 +5,15 @@
 #include <stdio.h>
 
 typedef double real_fn(double);
+typedef double complex complex_fn(double complex);
 
 static double const dx = 1E-10;
 
 double derivative_real(real_fn F, double x) {
+  return (F(x+dx) - F(x))/dx;
+}
+
+double complex derivative_complex(complex_fn F, double complex x) {
   return (F(x+dx) - F(x))/dx;
 }
 
@@ -21,12 +26,32 @@ double newton_real(real_fn F, double x) {
   return newton_real(F, x2);
 }
 
+double complex newton_complex(complex_fn F, double complex x) {
+  double complex fx = F(x);
+  if (cabs(fx) < dx) {
+    return x;
+  }
+  double complex x2 = x - fx/derivative_complex(F, x);
+  return newton_complex(F, x2);
+}
+
 double f1(double x) {
-  return x*x*x - 4*x*x + 1;
+  return x*x*x - 4.0*x*x + 1.0;
+}
+
+double complex f2(double complex x) {
+  return x*x*x - 4.0*x*x + 25.0*x - 100.0;
 }
 
 int main(void) {
   printf("f1(x) = x^3 - 4*x^2 + 1\n");
-  printf("f1(%g) = 0\n", newton_real(f1, 0.5));
+  double zer1 = newton_real(f1, 0.5);
+  double res1 = f1(zer1);
+  printf("f1(%g) = %g\n", zer1, res1);
+  printf("\n");
+  printf("f2(x) = x^3 - 4*x^2 + 25x - 100\n");
+  double complex zer2 = newton_complex(f2, -7.0*I);
+  double complex res2 = f2(zer2);
+  printf("f2(%g%+gi) = %g%+gi", creal(zer2), cimag(zer2), creal(res2), cimag(res2));
   return EXIT_SUCCESS;
 }

c62666efa683ee1b174141f3c29482cd2dabfc1d
newagebegins <newagebegins@gmail.com>
2023-04-17 19:08:00 +1000

    Handle simple brackets

--- my_regexp.c
+++ my_regexp.c
@@ -1,9 +1,28 @@
 #include <stdbool.h>
 #include <assert.h>
 
+bool match(char const* r, char const* s);
+
+char const* find_right_bracket(char const* r) {
+  while (*r && *r != ']') ++r;
+  return r;
+}
+
+bool match_bracket(char const* r, char const* s) {
+  switch (*r) {
+    case ']':
+      return false;
+    default:
+      if (*r == *s) {
+        r = find_right_bracket(r);
+        assert(r);
+        return match(r+1, s+1);
+      }
+      return match_bracket(r+1, s);
+  }
+}
+
 bool match(char const* r, char const* s) {
-  assert(r);
-  assert(s);
   if (!*r) {
     return true;
   }
@@ -21,6 +40,12 @@ bool match(char const* r, char const* s) {
     // Match any single character
     case '?': {
       return match(r+1, s+1);
+    }
+    case '[': {
+      return match_bracket(r+1, s);
+    }
+    case ']': {
+      assert(false);
     }
     // Match a literal character
     default: {
@@ -50,6 +75,15 @@ void test_match(void) {
   assert(!match("a?c", "ac"));
   assert(!match("a?c", "ab"));
   assert(!match("a?c", "bc"));
+
+  assert(match("a[cm]at", "acat"));
+  assert(match("[cm]at", "cat"));
+  assert(match("a[cm]at", "amat"));
+  assert(match("[cm]at", "mat"));
+  assert(!match("a[cm]at", "ahat"));
+  assert(!match("[cm]at", "hat"));
+  assert(!match("[cm]at", "at"));
+  assert(!match("[cm]at", "ca"));
 }
 
 int main(void) {

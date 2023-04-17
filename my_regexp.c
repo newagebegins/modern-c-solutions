158c0f2fa9a040769a235a756ece93b4730f77df
newagebegins <newagebegins@gmail.com>
2023-04-17 13:52:56 +1000

    Handle * to match anything

--- my_regexp.c
+++ my_regexp.c
@@ -10,8 +10,20 @@ bool match(char const* r, char const* s) {
   if (!*s) {
     return false;
   }
-  if (*r == *s) {
-    return match(r+1, s+1);
+  switch (*r) {
+    // Match anything
+    case '*': {
+      if (match(r+1, s)) {
+        return true;
+      }
+      return match(r, s+1);
+    }
+    // Match a literal character
+    default: {
+      if (*r == *s) {
+        return match(r+1, s+1);
+      }
+    }
   }
   return false;
 }
@@ -21,6 +33,13 @@ void test_match(void) {
   assert(!match("aabb", "abbb"));
   assert(!match("aabb", "c"));
   assert(!match("aabb", "a"));
+
+  assert(match("a*b", "ab"));
+  assert(match("a*b", "afoob"));
+  assert(match("a*b", "aabb"));
+  assert(!match("a*b", "afooc"));
+  assert(!match("a*b", "ac"));
+  assert(!match("a*b", "a"));
 }
 
 int main(void) {
